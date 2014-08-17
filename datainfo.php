ae9291db47b5e3fd659aa4e5279ca234fe1fd548
wellyton <wellyton.r@gmail.com>
2014-08-17 16:24:38 -0700

    Create datainfo.php

---
+++ datainfo.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+		<title>Chart</title>
+	</head>
+	<body>
+		<div id="header">
+			<div id="navbar">
+				<ul>
+					<li><a href="home.php">Home</a></li>
+					<li><a href="database.php">Database Table</a></li>
+					<li><a href="datainfo.php">Data Info</a></li>
+					<li><a href="aboutus.php">About Us</a></li>
+				</ul>
+			</div>
+		</div>
+		
+		<div class="center"> 
+			<div id="lastdata1">
+				<h5>Last data collected from sensor 1</h5>
+			</div>
+			<div id="lastdata2">
+				<h5>Last data collected from sensor 2</h5>
+			</div>
+			<div id="avgday1">
+				<h5>Average day temperature sensor 1</h5>
+			</div>
+			<div id="avgday2">
+				<h5>Average day temperature sensor 2</h5>
+			</div>
+		</div>
+
+		<div id="footer">
+			<p>Sense Group | <a class="email" href="mailto:wsn.group@u.washington.edu">wsn.group@u.washington.edu</a></p>
+		</div>
+	</body>
+</html>
