1da747d39040c7fc0b13b514976f72f8b3dce628
wellyton <wellyton.r@gmail.com>
2014-08-17 16:23:19 -0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+		<title>Temperature Sensor</title>
+	</head>
+	<body>
+		<div id="header">
+			<div id="navbar">
+				<ul>
+					<li><a href="home.php">Home</a></li>
+					<li><a href="database.php">Database Table</a></li>
+					<li><a href="datainfo.php">Data Info</a></li>
+					<li><a href="aboutus.php">About Us</a></li>
+				</ul>
+			</div>
+		</div>
+
+
+		<div class="center"> 
+			<h1>Sense Group - Wireless Sensor Network</h1>
+			<h2>This project has as goal capture the temperature using two sensor. We are using the zigbee protocol to 
+				send the data from the sensor board to the Raspberry Pi. And finaly, we store all data in the cloud - Microsoft Azure.</h2>
+		</div>
+
+		<div id="footer">
+			<p>Sense Group | <a class="email" href="mailto:wsn.group@u.washington.edu">wsn.group@u.washington.edu</a></p>
+		</div>
+	</body>
+</html>
