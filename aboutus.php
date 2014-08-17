2280734f6d4d6db97d0f5871d876825a3191e299
wellyton <wellyton.r@gmail.com>
2014-08-17 16:24:57 -0700

    Create aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+		<title>About Us</title>
+	</head>
+	<body>
+		<div id="header">
+			<div id="navbar">
+				<ul>
+					<li><a href="home.php">Home</a></li>
+					<li><a href="database.php">Database Table</a></li>s
+					<li><a href="datainfo.php">Data Info</a></li>
+					<li><a href="aboutus.php">About Us</a></li>
+				</ul>
+			</div>
+		</div>
+		
+		<div class="center"> 
+			
+		</div>
+
+		</div>
+		<div id="footer">
+			<p>Sense Group | <a class="email" href="mailto:wsn.group@u.washington.edu">wsn.group@u.washington.edu</a></p>
+		</div>
+	</body>
+</html>
